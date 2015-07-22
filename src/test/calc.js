9c4438083e24b979fff6dab4f8494ff6701554f7
GuillaumeSavaton <guillaume.savaton@eseo.fr>
2015-07-22 10:55:16 +0200

    Renamed main module

--- src/test/calc.js
+++ src/test/calc.js
@@ -1,4 +1,4 @@
-import * as earley from "../lib/earley";
+import * as persil from "../lib/persil";
 
 var _ = 0;
 var INT = 1;
@@ -95,7 +95,7 @@ var calc = {
 };
 
 var str = "56 + 37*2 - (8 /";//75 + 904 )";
-var res = earley.parse(calc, START, str);
+var res = persil.parse(calc, START, str);
 
 if (res.error) {
     console.log("Parse error at " + res.location);
