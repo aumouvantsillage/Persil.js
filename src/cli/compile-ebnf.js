e457e5a3d27ddebce50b8f9b33f4089244f4bdca
GuillaumeSavaton <guillaume.savaton@eseo.fr>
2015-08-18 15:12:20 +0200

    Added command-line tools

---
+++ src/cli/compile-ebnf.js
@@ -0,0 +1,18 @@
+
+import {compile} from "../lib/ebnf";
+import {stringify} from "../lib/persil";
+import {error} from "../lib/logging";
+import fs from "fs";
+
+const args = process.argv.slice(2);
+
+fs.readFile(args[0], {encoding: "utf-8"}, (err, data) => {
+    if (err) {
+        throw err;
+    }
+    const res = compile(data);
+    if (res.error) {
+        error(data, res);
+    }
+    console.log(stringify(res.data));
+});
